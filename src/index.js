55fb2b01d186d5844ad71593f60f5559748653a6
nicolly-rocha <nicolly.rocha@ibm.com>
2022-06-28 16:01:48 -0300

    Props - destructuring

--- src/index.js
+++ src/index.js
@@ -1,47 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//setup variables
+const firstBook = {
+  title: 'A revolução dos bichos: Um conto de fadas',
+  author: 'George Orwell, Heitor Aquino Ferreira',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61owA5ey3iL._AC_SX368_.jpg',
+};
+const secondBook = {
+  title: 'Leitura de verão',
+  author: 'Emily Henry',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71KnjPmQnkL.jpg',
+};
 
 function BookList() {
   return (
     <section>
-      <Book />
+      <Book
+        img={firstBook.img}
+        author={firstBook.author}
+        title={firstBook.title}
+      />
+      <Book
+        img={secondBook.img}
+        author={secondBook.author}
+        title={secondBook.title}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
+  //const { img, title, author } = props;
   return (
     <div className="div-article">
       <article className="article">
-        <Image />
-        <Title className="title" />
-        <Author />
-        <Capa />
+        <img src={img} alt="" className="imagem" />
+        <h1 className="title">{title}</h1>
+        <h4 className="author">{author}</h4>
       </article>
     </div>
   );
-};
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/61owA5ey3iL._AC_SX368_.jpg"
-      alt=""
-      className="imagem"
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>A revolução dos bichos: Um conto de fadas</h1>;
-};
-
-const Author = () => {
-  return <h4 className="author">George Orwell, Heitor Aquino Ferreira</h4>;
-};
-
-const Capa = () => {
-  return <div className="capa">Capa comum</div>;
 };
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
